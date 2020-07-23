c6acd44b99f8dd95104475bccaaba2fa0a860bbb
dotja <60714715+dotja@users.noreply.github.com>
2020-07-23 16:16:57 +0100

    Update jobs.js

--- static/jobs.js
+++ static/jobs.js
@@ -16,22 +16,22 @@
     send_request(param);
   }
 
-function send_request(param) {
-	$.ajax({
-		method: 'GET',
-		url: 'api/get_jobs?' + param,
-		beforeSend: function() {
-			console.log('before send');
-		},
-		success: function(result) {
-			update_table(result);
-			console.log('after send');
-		},
-		error: function() {
-			console.log('error');
-		}
-	});
-}
+  function send_request(param) {
+    $.ajax({
+      method: 'GET',
+      url: 'api/get_jobs?' + param,
+      beforeSend: function() {
+        console.log('before send');
+      },
+      success: function(result) {
+        update_table(result);
+        console.log('after send');
+      },
+      error: function() {
+        console.log('error');
+      }
+    });
+  }
 
 
   function update_table(data) {
